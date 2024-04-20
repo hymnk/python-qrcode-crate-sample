ddcee295dc7432bd4a67d254433419f7822b9bc0
hymnk3 <ymnk8810@gmail.com>
2024-04-20 22:23:02 +0900

    fix double quotes

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import qrcode
 import os
 
 url = "https://github.com/hymnk/python-qrcode-create-sample"
-output_dir = 'results'
-output_img_filename = 'qr.png'
+output_dir = "results"
+output_img_filename = "qr.png"
 
 qr = qrcode.QRCode(
     version=1,
@@ -19,5 +19,5 @@ img = qr.make_image(fill_color="black", back_color="white")
 if not os.path.isdir(output_dir):
     os.mkdir(output_dir)
 
-qr_code_path = output_dir + '/' + output_img_filename
+qr_code_path = output_dir + "/" + output_img_filename
 img.save(qr_code_path)
